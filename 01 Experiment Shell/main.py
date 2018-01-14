9a6c3d78b552081d2d59f23bf2df15bf24a92ca2
Jonathan Comeau <jon.comeau@gmail.com>
2018-01-13 20:24:01 -0700

    changed .gitignore (last commit didn't have it)

--- 01 Experiment Shell/main.py
+++ 01 Experiment Shell/main.py
@@ -1,13 +1,22 @@
 import data
 from hard_coded_classifier import HardCodedClassifier
 
+
+def mean(numbers):
+    return float(sum(numbers)) / max(len(numbers), 1)
+
+
 dat = data.load()
 x_train, x_test, y_train, y_test = data.split_seventy_thirty(dat.data, dat.target)
-model = data.gaussian_nb(x_train, y_train)
-prediction = data.predict(model, x_test)
-accuracy = data.score(y_test, prediction)
 
-print('Accuracy of GaussianNB:', accuracy)
+accuracies = []
+
+for i in range(100):
+    model = data.gaussian_nb(x_train, y_train)
+    prediction = data.predict(model, x_test)
+    accuracies.append(data.score(y_test, prediction))
+
+print('Accuracy of GaussianNB:', mean(accuracies))
 
 hard_model = HardCodedClassifier.fit(x_train, y_train)
 hard_prediction = HardCodedClassifier.predict(hard_model, x_test)
