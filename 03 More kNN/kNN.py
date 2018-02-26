Fixed normalization problems

03 More kNN/kNN.py
@@ -22,32 +22,21 @@ class KNNClassifier:
         self.train_data = train_data.astype(dtype=np.float)
         self.train_target = train_target
 
-        columns = self.train_data.T
-
-        for col in columns:
-            self.get_max_min(col)
-
-        print(self.max_min)
-
-        for index in range(len(columns)):
-            for ind in range(len(columns[index])):
-                value = float(columns[index][ind])
-                col_min = float(self.max_min[index]["min"])
-                col_max = float(self.max_min[index]["max"])
-                total = (value - col_min) / (col_max - col_min)
-
-                columns[index][ind] = total
-
-        print(self.train_data)
-
-    def get_max_min(self, col):
-        col_min = min(col)
-        col_max = max(col)
-
-        self.max_min.append({"max": col_max, "min": col_min})
-
     def predict(self, test_data):
         predictions = []
+
+        neighbors_list = self.nearest_neighbors(test_data)
+
+        for neighbors in neighbors_list:
+            counter = Counter(neighbors)
+            most_common = counter.most_common(1)
+
+            predictions.append(most_common[0][0])
+
+        return predictions
+
+    def nearest_neighbors(self, test_data):
+        neighbor_list = []
 
         for test_row in test_data:
             distances = []
@@ -62,14 +51,9 @@ class KNNClassifier:
             for index in range(self.k):
                 neighbors.append(self.train_target[distances[index][1]])
 
-            counter = Counter(neighbors)
-            most_common = counter.most_common(1)
+            neighbor_list.append(neighbors)
 
-            print(neighbors)
-
-            predictions.append(most_common[0][0])
-
-        return predictions
+        return neighbor_list
 
     def score(self, test_data, test_classes):
         predicted = self.predict(test_data)

03 More kNN/main.py
@@ -1,6 +1,7 @@
 from sklearn.preprocessing import LabelEncoder
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import train_test_split
+from sklearn.datasets import load_iris
 from datasets import download, load
 from kNN import KNNClassifier
 
@@ -102,6 +103,15 @@ cols = {
 download()
 
 datas = load(cols)
+iris = load_iris()
+
+# testing iris
+
+irisDF = pd.DataFrame(data=np.c_[iris['data'], iris['target']], columns=iris['feature_names'] + ['target'])
+
+print("Iris Dataset")
+
+calculate_accuracy(irisDF, "target")
 
 # cars
 
@@ -109,9 +119,9 @@ process_cars(datas['cars'])
 
 print("Cars Dataset")
 
-# calculate_accuracy(datas['cars'], "class-values")
+calculate_accuracy(datas['cars'], "class-values")
 
-# pima
+# pima indian
 
 process_pima(datas['pima'], ['glucose-concentration',
                              'diastolic-blood-pressure',
@@ -120,9 +130,6 @@ process_pima(datas['pima'], ['glucose-concentration',
                              'diabetes-pedigree-function',
                              'two-hour-insulin',
                              'age'])
-
-print(datas['pima'])
-print("")
 
 print("Pima Dataset")
 